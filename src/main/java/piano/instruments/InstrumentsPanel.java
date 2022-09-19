061598ab111d07de3b27727cc15b020144586c31
MDreyfuss <76665915+MDreyfuss@users.noreply.github.com>
2022-09-18 23:50:06 -0400

    Refactored instrument data and added instruments

--- src/main/java/piano/instruments/InstrumentsPanel.java
+++ src/main/java/piano/instruments/InstrumentsPanel.java
@@ -11,9 +11,19 @@ public class InstrumentsPanel extends JPanel {
         setLayout(new FlowLayout());
         setSize(MainFrameInterface.KEYBOARD_WIDTH, MainFrameInterface.INSTRUMENTS_PANEL_HEIGHT);
         setBackground(Color.BLACK);
-        for (InstrumentButton instrumentButton: new InstrumentButtonsArray()) {
-            instrumentButton.addActionListener(e -> midiChannel.programChange(instrumentButton.getInstrument()));
-            add(instrumentButton);
+        JComboBox combo = new JComboBox();
+
+
+        //instrument data from https://www.midi.org/specifications-old/item/gm-level-1-sound-set
+        String[] instrumentName = {"Acoustic Grand Piano", "Music Box", "Xylophone", "Accordion",
+                "Harmonica", "Acoustic Guitar", "Electric Guitar", "Violin", "Cello", "Trumpet",
+                "Flute"};
+        int[] instrumentVal = {1, 11, 14, 22, 23, 25, 27, 41, 43, 57, 74};
+
+        for (String next: instrumentName) {
+            combo.addItem(next);
         }
+        combo.addActionListener(e -> midiChannel.programChange(instrumentVal[combo.getSelectedIndex()]));
+        add(combo);
     }
 }

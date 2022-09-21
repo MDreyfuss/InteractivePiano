760dfb4d8c7289a1c0b93f25688842a42cf7ad09
MDreyfuss <76665915+MDreyfuss@users.noreply.github.com>
2022-09-20 20:52:01 -0400

    removed unecessary try-catch

--- src/main/java/piano/instruments/InstrumentList.java
+++ src/main/java/piano/instruments/InstrumentList.java
@@ -8,23 +8,18 @@ public class InstrumentList {
 
     private ArrayList<Instrument> instruments = new ArrayList<>();
 
-    public InstrumentList()
-    {
+    public InstrumentList() {
         //instrument data from https://www.midi.org/specifications-old/item/gm-level-1-sound-set
 
-        try
-        {
-            InputStream list = InstrumentList.class.getResourceAsStream("ListOfInstruments.txt");
+        InputStream list = InstrumentList.class.getResourceAsStream("ListOfInstruments.txt");
 
-            Scanner readFile = new Scanner(list);
-            while (readFile.hasNextLine()) {
-                String line = readFile.nextLine();
-                String[] entry = line.split(" ", 2);
-                instruments.add(new Instrument(entry[1], Integer.valueOf(entry[0])));
-            }
-        } catch (Exception e){
-            e.printStackTrace();
+        Scanner readFile = new Scanner(list);
+        while (readFile.hasNextLine()) {
+            String line = readFile.nextLine();
+            String[] entry = line.split(" ", 2);
+            instruments.add(new Instrument(entry[1], Integer.valueOf(entry[0])));
         }
+
     }
 
     public ArrayList<Instrument> getInstruments ()
